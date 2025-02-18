cc01aa3a808559757e2828351db97cefbfd67e8c
Ste O'Brien <stephan.obrien@mcgill.ca>
2025-02-18 11:54:20 -0500

    Adding formating for analyze.py

--- analyze.py
+++ analyze.py
@@ -1,14 +1,19 @@
-from calculations import *
+from calculations import (
+    add,
+    sub,
+    mult,
+    div,
+)
 import sys
 
 a = float(sys.argv[1])
 b = float(sys.argv[2])
 
-print (f"a = {a}")
-print (f"b = {b}")
+print(f"a = {a}")
+print(f"b = {b}")
 
 # Print out the various operations
-print (f"a + b = {add(a,b)}")
-print (f"a - b = {sub(a,b)}")
-print (f"a * b = {mult(a,b)}")
-print (f"a / b = {div(a,b)}")
+print(f"a + b = {add(a, b):0.3f}")
+print(f"a - b = {sub(a, b):0.3f}")
+print(f"a * b = {mult(a, b):0.3f}")
+print(f"a / b = {div(a, b):0.3f}")
